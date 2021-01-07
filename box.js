3032e4598ce75b1e58bc9996086dffbc6f0dd580
Anu-Marwaha <anumarwaha2010@gmail.com>
2021-01-07 14:02:59 +0530

    error resolved

--- box.js
+++ box.js
@@ -1,15 +1,12 @@
 class Box{
-    constructor(x,y,width,height,angle){
+    constructor(x,y,width,height){
         var boxOption=
-           { isStatic:true,
-            restitution:0.8,
-        friction:0.3,
-    denstity:0.4}
+           { isStatic:true
+        }
         
         this.body=Bodies.rectangle(x,y,width,height,boxOption);
         World.add(myWorld,this.body);
-        this.x=x;
-        this.y=y;
+      
         this.height=height;
         this.width=width;
     }
@@ -21,6 +18,7 @@ class Box{
         translate(this.body.position.x,this.body.position.y);
         rotate(angle);
         rectMode(CENTER);
+      //  rect(this.body.position.x,this.body.position.y,this.width,this.height);
       rect(0,0,this.width,this.height);
        pop();
     }
--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 var myEngine, myWorld
 var box1, box2, box3;
-
+var gound1,groundSprite1;
 function preload()
 {
 	helicopterIMG=loadImage("helicopter.png")
@@ -19,31 +19,41 @@ function setup() {
 	
 	myEngine=Engine.create();
 	myWorld=myEngine.world;
-	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite=createSprite(width/2, 200, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
+	
+	;
 
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 
-	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite=createSprite(width/2, height-20, width,10);
 	groundSprite.shapeColor=color(255)
 
 
-	engine = Engine.create();
-	world = engine.world;
+//	engine = Engine.create();
+//	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:true});
-	World.add(world, packageBody);
+	packageBody = Bodies.rectangle(width/2,200,10,10,{restitution:0.3, isStatic:true});
+	World.add(myWorld, packageBody);
 	
 
 	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-    box1=new Box(width/2,650,200,20);
+	ground = Bodies.rectangle(width/2, height-20, width, 10 , {isStatic:true} );
+ 	World.add(myWorld, ground);
+    box1=new Box(400,650,200,20);
 	box2=new Box(500,550,20,200);
 	box3=new Box(300,550,20,200);
+
+//	ground1 = Bodies.rectangle(width/2, height-150, width, 10 , {isStatic:true} );
+//	 World.add(myWorld, ground1);
+//	 groundSprite1=createSprite(width/2, height-150, width,10);
+//	groundSprite1.shapeColor=color("blue");
+
+	//console.log(box1.depth);
+	//console.log(packageSprite.depth);
     
 }
 
@@ -51,20 +61,21 @@ function setup() {
 function draw() {
   background(0);
   Engine.update(myEngine);
-  packageSprite.x= packageBody.position.x ;
-  packageSprite.y= packageBody.position.y ;
-  keyPressed();
+ 
+ // keyPressed();
   box1.display();
   box2.display();
   box3.display();
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y ;
   drawSprites();
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     Matter.Body.setStatic(packageBody, false);
-    packageSprite.x= packageBody.position.x ;
-  packageSprite.y= packageBody.position.y ;
+ //   packageSprite.x= packageBody.position.x ;
+ // packageSprite.y= packageBody.position.y ;
   }
 }
 
